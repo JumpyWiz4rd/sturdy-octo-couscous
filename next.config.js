2e7ede1a2a1d70f6d66a73df62ec0f8cb87fa869
Alex <alexkriv1994@gmail.com>
2023-07-17 22:30:18 +0300

    agai1n!

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: ''
+    basePath: '',
+    images: {
+      loader: 'akamai',
+      path: '',
+    },
   }
   
 
